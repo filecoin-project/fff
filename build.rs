5d06836b1602e30320ad29f7a11e5b32fb424728
寧靜 <luozijun.assistant@protonmail.com>
2020-05-07 14:56:50 +0800

    fix compilation error on arm platform.

--- build.rs
+++ build.rs
@@ -1,9 +1,18 @@
+
+
 fn main() {
-    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    if target_arch == "x86_64" {
-        cc::Build::new()
-            .flag("-c")
-            .file("./asm/mul_4.S")
-            .compile("libff-derive-crypto.a");
+    if let Ok(target_arch) = std::env::var("CARGO_CFG_TARGET_ARCH") {
+        match target_arch.as_ref() {
+            "x86_64" => {
+                println!("cargo:rerun-if-changed=./asm/mul_4.S");
+                cc::Build::new()
+                    .flag("-c")
+                    .file("./asm/mul_4.S")
+                    .compile("libff-derive-crypto.a");
+            },
+            _ => {
+
+            },
+        }
     }
 }
