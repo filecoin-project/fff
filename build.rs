79583156f0ec3683c197fe7955e763c7e34ed776
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2022-01-02 10:10:56 +0000

    Also compile assembly in cases of x64 processors

--- build.rs
+++ build.rs
@@ -10,5 +10,17 @@ fn main() {
     }
 }
 
+#[cfg(target_arch = "x64")]
+fn main() {
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+
+    if target_arch == "x64" {
+        cc::Build::new()
+            .flag("-c")
+            .file("./asm/mul_4.S")
+            .compile("libff-derive-crypto.a");
+    }
+}
+
 #[cfg(not(target_arch = "x86_64"))]
 fn main() {}
